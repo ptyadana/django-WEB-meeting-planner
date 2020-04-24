59f96f68ee008fd52d694b94105ac2179160552a
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-04-24 14:25:05 +0800

    website app

--- meeting_planner_project/urls.py
+++ meeting_planner_project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from website import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.welcome),
+    path('date/',views.date),
+    path('about/',views.about)
 ]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('welcome to meeting planner')
+
+def date(request):
+    return HttpResponse(f'This page was served at {datetime.now()}')
+
+def about(request):
+    return HttpResponse('Hello this is Phone Thiri, rocking with Django!')
