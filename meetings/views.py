4e9fefdf61cf163e6ffe17d8947b5727956eb513
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-04-24 21:57:49 +0800

    add user interaction with forms

--- meetings/views.py
+++ meetings/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
+# from django.forms import modelform_factory
+
 from meetings.models import Meeting, Room
+from .forms import MeetingForm
+
 
 def detail(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
@@ -9,3 +13,26 @@ def rooms_list(request):
     num_rooms = Room.objects.count()
     rooms = Room.objects.all()
     return render(request, 'meetings/rooms_list.html', {'num_rooms':num_rooms,'rooms':rooms})
+
+# Approach 1
+# MeetingForm = modelform_factory(Meeting, exclude=[])
+# def new(request):
+#     if request.method == 'POST':
+#         form = MeetingForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('welcome')
+#     else:
+#         form = MeetingForm()
+#     return render(request, 'meetings/new_meeting.html', {'form':form})
+
+# Approach 2
+def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new_meeting.html", {"form": form})
